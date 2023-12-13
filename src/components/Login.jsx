46bd34413e82731c62dedab51401c0d6370977a5
= <antoniochavezmarco@gmail.com>
2023-12-13 10:07:44 -0600

    Login added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./css/forms.css";
+import "./css/page.css";
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -7,7 +10,8 @@ function Login() {
         <main>
             <img src="/img/logo.png" alt="Logo Upemor" id="logo_upemor" />
             <form>
-                <h1>Iniciar sesión</h1>
+                <img src="/img/user_big.png" alt="Icono de usuario" id="icon_user" />
+                <h1>Inicio de sesión</h1>
                 <div className="inputForm">
                     <label htmlFor="email"><img src="/img/user.png" alt="Icono de usuario" /></label>
                     <input type="email" name="email" id="email" placeholder="Ingresa tu correo electrónico" onChange={(event)=>(setEmail(event.target.value))} required/>
@@ -17,6 +21,8 @@ function Login() {
                     <input type={`${showPassword ? "text" : "password"}`} name="password" id="password" placeholder="Ingresa tu contraseña" onChange={(event)=>(setPassword(event.target.value))}required/>
                     <button type="button" onClick={() => (setShowPassword(!showPassword))}><img src={`/img/${showPassword ? "ver.png" : "no_ver.png"}`} alt="Icono para cambiar visibilidad" /></button>
                 </div>
+                <p>¿Olvidaste tu contraseña?<Link to="/recuperacion">Recupérala dando clic aquí</Link></p>
+                <button className="login">Iniciar sesión</button>
             </form>
         </main>
     );
