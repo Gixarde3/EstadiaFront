374f5c29cb93af7bf570f5169f32ae0dcfabfb5f
= <antoniochavezmarco@gmail.com>
2024-03-19 20:44:49 -0600

    Boton añadido

--- src/components/GestionUsuarios.jsx
+++ src/components/GestionUsuarios.jsx
@@ -1,12 +1,17 @@
 import Perfiles from "./Perfiles";
 import Cookies from "js-cookie";
+import { useState } from "react";
 import Usuarios from "./Usuarios";
 function GestionUsuarios() {
+    const [editar, setEditar] = useState(false);
     const tipoUsuario = Cookies.get("tipoUsuario");
     return (<>
         <Usuarios />
         {
-           tipoUsuario === "3" ? <Perfiles/> : <></>
+            tipoUsuario === "3" &&  <button className="login" onClick={()=>setEditar(!editar)}>Registrar usuario</button>
+        }
+        {
+            editar === true && <Perfiles/> 
         }
     </>);
 }
